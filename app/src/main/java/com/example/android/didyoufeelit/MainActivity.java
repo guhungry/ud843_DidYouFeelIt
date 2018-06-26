c06673f3b1ac0f1f1755d975caa7196300a5601c
Woraphot Chokratanasombat <wc@asiola.com>
2018-06-26 12:06:59 +0700

    [Main Activity] Change: Move to background thread

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -34,11 +35,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        //new EarthQuakeTask().execute(USGS_REQUEST_URL);
+        new EarthQuakeTask().execute();
     }
 
     /**
@@ -53,5 +51,21 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class EarthQuakeTask extends AsyncTask<String, Void, Event> {
+        @Override
+        protected Event doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) return null;
+
+            return Utils.fetchEarthquakeData(urls[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            super.onPostExecute(event);
+
+            if (event != null) updateUi(event);
+        }
     }
 }
